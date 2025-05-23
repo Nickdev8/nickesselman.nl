b965836305566f77058d9e95264c3748a75bb152
Nick Esselman <nick.esselman@gmail.com>
2025-05-23 18:33:48 +0200

    added hackpadstory

--- projects.php
+++ projects.php
@@ -9,6 +9,13 @@ $projects = [
         'basiclayout' => 'false',
     ],
     [
+        'title' => 'Party VR',
+        'description' => 'This is the game i made for juice',
+        'image' => 'partyvr.jpg',
+        'link' => 'partyvr',
+        'basiclayout' => 'false',
+    ],
+    [
         'title' => 'My tree',
         'description' => 'This is an idea i had for visualising the prjects i made',
         'image' => 'mytree.jpg',
@@ -16,10 +23,67 @@ $projects = [
         'basiclayout' => 'false',
     ],
     [
-        'title' => 'Project 2',
-        'description' => 'Description of project 2',
-        'image' => '2.jpg',
-        'link' => 'project2',
+        'title' => 'HackPad',
+        'description' => 'I Designed, Solderd and Programmed a Micropad',
+        'image' => 'hackpad.jpg',
+        'link' => 'hackpad',
+        'basiclayout' => 'true',
+        'blocks' => [
+            [
+                'image' => [
+                    'images/hackpad/pcbfront.jpg',
+                ]
+            ],
+            [
+                'title' => 'Overview',
+                'content' => '
+                <p>
+                This was a project i made with HackClub. <br>
+                The event was: "Design a pcb for a micropad, and recieve the parts for it"<br>
+                I made a pcb with a 4x4 matrics of Cherry MX Switches.<br>
+                Find more information in my github repo <a href="https://github.com/Nickdev8/macropad">here</a><br>
+                <br>
+                I also made a Case for it in fusion 360, <br>
+                this all took me only 1 days of work. <br>
+                <br>
+                I solderd this together with my grandpa, he teatched me how to solder.<br>
+                and after only an hour or 2, I had a finished board.<br>
+                Then came the programming. <br>and with support from chatGPT i had it working on notime.
+                </p>
+                ',
+                'image' => [
+                    'images/hackpad/fusion.png',
+                ]
+            ],
+            [
+                'title' => 'BOM',
+                'content' => '
+                <ul>
+                    <li>2x SK6812 MINI Leds</li>
+                    <li>1x XIAO RP2040</li>
+                    <li>16x Blank DSA Keycaps</li>
+                    <li>4x M3x16mm Bolt</li>
+                    <li>4x M3 Heatset</li>
+                    <li>16x 1N4148 Diodes</li>
+                </ul>
+                <p>feel free to copy the files, modify it and make your own.',
+                'image' => [
+                    'images/hackpad/schematic.png',
+                ]
+            ],
+            [
+                'image' => [
+                    'images/hackpad/pcbback.jpg',
+                ]
+            ],
+
+        ],
+    ],
+    [
+        'title' => 'template',
+        'description' => 'Description of template',
+        'image' => 'template.jpg',
+        'link' => 'template',
         'basiclayout' => 'true',
         'blocks' => [
             [
--- projects/basiclayout.php
+++ projects/basiclayout.php
@@ -4,29 +4,62 @@ include_once 'basics.php';
 ?>
 <?php if (!empty($selectedProject['blocks'])): ?>
     <?php foreach ($selectedProject['blocks'] as $block): ?>
-        <div class="card container separator">
-            <?php if (!empty($block['title'])): ?>
-                <h2 class="headline"><?= htmlspecialchars($block['title']) ?></h2>
+
+        <?php
+        $hasImage = !empty($block['image']);
+        $hasTitle = !empty($block['title']);
+        $hasSubtitle = !empty($block['subtitle']);
+        $hasContent = !empty($block['content']);
+        $isOnlyImage = $hasImage && !($hasTitle || $hasSubtitle || $hasContent);
+
+        $wrapperClass = $isOnlyImage
+            ? 'wide container separator img-wide'
+            : 'card container separator';
+        ?>
+
+        <div class="<?= $wrapperClass ?>">
+
+            <?php if (!$isOnlyImage): ?>
+                <?php if ($hasTitle): ?>
+                    <h2 class="headline"><?= htmlspecialchars($block['title']) ?></h2>
+                <?php endif; ?>
+                <?php if ($hasSubtitle): ?>
+                    <h3 class="lead"><?= htmlspecialchars($block['subtitle']) ?></h3>
+                <?php endif; ?>
             <?php endif; ?>
-            <?php if (!empty($block['subtitle'])): ?>
-                <h3 class="lead"><?= htmlspecialchars($block['subtitle']) ?></h3>
-            <?php endif; ?>
-            <?php if (!empty($block['image'])): ?>
-                <div class="split">
-                    <div>
-                        <?= $block['content'] ?? '' ?>
-                    </div>
-                    <div>
-                        <img src="<?= htmlspecialchars($block['image']) ?>"
-                             alt="<?= htmlspecialchars($block['title'] ?? 'Block image') ?>"
-                             class="img-cropped">
-                    </div>
-                </div>
-            <?php else: ?>
+
+            <?php if (!$hasImage): ?>
                 <div>
                     <?= $block['content'] ?? '' ?>
                 </div>
+
+            <?php else: ?>
+                <?php
+                $images = is_array($block['image'])
+                    ? $block['image']
+                    : [$block['image']];
+                ?>
+
+                <?php if ($isOnlyImage): ?>
+                    <?php foreach ($images as $img): ?>
+                        <img src="<?= htmlspecialchars($img) ?>" alt="" class="img-full">
+                    <?php endforeach; ?>
+
+                <?php else: ?>
+                    <div class="split">
+                        <div>
+                            <?= $block['content'] ?? '' ?>
+                        </div>
+                        <div>
+                            <?php foreach ($images as $img): ?>
+                                <img src="<?= htmlspecialchars($img) ?>" alt="<?= htmlspecialchars($block['title'] ?? 'Block image') ?>"
+                                    style="border-radius: 2rem;">
+                            <?php endforeach; ?>
+                        </div>
+                    </div>
+                <?php endif; ?>
             <?php endif; ?>
+
         </div>
     <?php endforeach; ?>
 <?php endif; ?>
