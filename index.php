1c12ab4338c80b05034c3e8b0205b7a16ad94c20
Nick Esselman <nick.esselman@gmail.com>
2025-05-25 13:36:30 +0200

    :)

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $versionnum = "1.0.5"
-?>
+    ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -8,14 +8,17 @@ $versionnum = "1.0.5"
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $_GET['page']?></title>
+    <title><?= $_GET['page'] ?></title>
     <link rel="stylesheet" href="https://css.hackclub.com/theme.css" />
     <link rel="icon" type="image/png" href="./images/logo.png">
     <script type="module" src="scripts/loadmoreimages.js" defer></script>
-    
+
+    <!-- gsap -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
+
     <!-- basic css -->
-    <link rel="stylesheet" href="css/reset.css?v=<?=$versionnum?>">
-    <link rel="stylesheet" href="css/main.css?v=<?=$versionnum?>">
+    <link rel="stylesheet" href="css/reset.css?v=<?= $versionnum ?>">
+    <link rel="stylesheet" href="css/main.css?v=<?= $versionnum ?>">
 
     <!-- add css of the page Im on -->
     <?php
@@ -25,11 +28,11 @@ $versionnum = "1.0.5"
     $cssFile = "css/{$page}.css";
     if (file_exists($cssFile)):
         ?>
-        <link rel="stylesheet" href="<?php echo $cssFile; ?>?v=<?=$versionnum?>">
-    <?php endif; 
-    
+        <link rel="stylesheet" href="<?php echo $cssFile; ?>?v=<?= $versionnum ?>">
+    <?php endif;
+
     if ($page == "home"): ?>
-      <script src="scripts/home.js" defer></script>
+        <script src="scripts/home.js" defer></script>
     <?php endif; ?>
 
 </head>
@@ -40,48 +43,48 @@ $versionnum = "1.0.5"
     include 'pages/specials/header.php';
     ?>
     <main class="inner-main">
-    <?php
-    $page = $_GET['page'] ?? null;
-    $project = $_GET['project'] ?? null;
+        <?php
+        $page = $_GET['page'] ?? null;
+        $project = $_GET['project'] ?? null;
 
-    if ($page === null && $project === null) {
-        $page = 'home';
-    }
-
-    if ($page !== null) {
-        if (file_exists("pages/$page.php")) {
-            include "pages/$page.php";
-        } else {
-            include 'pages/specials/404.php';
+        if ($page === null && $project === null) {
+            $page = 'home';
         }
-    } else if ($project !== null) {
-        $selectedProject = null;
-        foreach ($projects as $proj) {
-            if ($proj['link'] === $project) {
-                $selectedProject = $proj;
-                break;
+
+        if ($page !== null) {
+            if (file_exists("pages/$page.php")) {
+                include "pages/$page.php";
+            } else {
+                include 'pages/specials/404.php';
             }
-        }
+        } else if ($project !== null) {
+            $selectedProject = null;
+            foreach ($projects as $proj) {
+                if ($proj['link'] === $project) {
+                    $selectedProject = $proj;
+                    break;
+                }
+            }
 
-        if ($selectedProject) {
-            if (isset($selectedProject['basiclayout']) && $selectedProject['basiclayout'] === 'true') {
-                include 'projects/basiclayout.php';
-            } else if (file_exists("projects/$project.php")) {
-                include "projects/$project.php";
+            if ($selectedProject) {
+                if (isset($selectedProject['basiclayout']) && $selectedProject['basiclayout'] === 'true') {
+                    include 'projects/basiclayout.php';
+                } else if (file_exists("projects/$project.php")) {
+                    include "projects/$project.php";
+                } else {
+                    include 'pages/specials/404.php';
+                }
             } else {
                 include 'pages/specials/404.php';
             }
         } else {
-            include 'pages/specials/404.php';
+            include 'pages/home.php';
         }
-    } else {
-        include 'pages/home.php';
-    }
-    ?>
+        ?>
     </main>
     <?php
     include 'pages/specials/footer.php';
     ?>
-</body>
+    </body>
 
 </html>
--- pages/home.php
+++ pages/home.php
@@ -6,17 +6,21 @@ include 'pages/specials/header.php';
 
 <div class="main-top">
     <h1 class="ultratitle">Nick Essleman</h1>
-    <h2 class="whatcanido">
+    <div class="card-3d">
+        <link rel="stylesheet" href="css/luanguagesamination.css">
+        <?php include 'pages/specials/alltheprogramingluangages.html'; ?>
+    </div>
+
+    <!-- <h2 class="whatcanido">
         <div class="whatcanido--card">
             <div class="whatcanido--loader">
                 <h2>I am a pasianate full stack programmer, with a very wide skillset</h2>
                 <div class="whatcanido--words">
-                    <link rel="stylesheet" href="css/luanguagesamination.css">
-                    <?php include 'pages/specials/alltheprogramingluangages.html';?>
+                    ?php include 'pages/specials/alltheprogramingluangages.html';?>
                 </div>
             </div>
         </div>
-    </h2>
+    </h2> -->
 </div>
 
 <div class="container split separator card">
@@ -53,3 +57,14 @@ include './pages/projects.php';
         <img class="modal-content" id="modalImg" src="" alt="Preview">
     </div>
 </div>
+
+
+<script>
+  gsap.from(".ultratitle", {
+    duration: 1.5,
+    y: -50,
+    opacity: 0,
+    ease: "bounce.out",
+    delay: 0.5
+  });
+</script>
