ce90a5c7f681230ea686276cce8bdef179458ecc
Nick <nick.esselman@gmail.com>
2025-08-07 11:07:58 +0200

    updateipns

--- pages/redirecttoblogwebsite.php
+++ pages/redirecttoblogwebsite.php
@@ -1,4 +1,5 @@
-<?php
-header("Location: https://blog.nickesselman.nl");
-exit();
-?>
+<div style="text-align: center; padding: 50px;">
+    <h1>My Blog has moved!</h1>
+    <p>Please click the link below to go to my blog.</p>
+    <p><a href="https://blog.nickesselman.nl">Go to blog.nickesselman.nl</a></p>
+</div>
